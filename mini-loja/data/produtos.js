80b2afd2a31c4c47d9dc282cb36b8803e86dd7da
manoela-oliveira <manoela.oliveira0900@gmail.com>
2026-08-14 17:27:46 -0300

    adicionando caminho para imagens

--- mini-loja/data/produtos.js
+++ mini-loja/data/produtos.js
@@ -1,9 +1,8 @@
-export const produtos = [
-  { id: '1', nome: '🎧 Fone Bluetooth', preco: 199.90 },
-  { id: '2', nome: '⌨️ Teclado Mecânico', preco: 349.90 },
-  { id: '3', nome: '🖱️ Mouse Gamer', preco: 159.90 },
-  { id: '4', nome: '🖥️ Monitor Ultrawide', preco: 1299.90 },
-  { id: '5', nome: '🎮 Controle sem Fio', preco: 299.90 },
-  { id: '6', nome: '📹 Webcam Full HD', preco: 189.90 },
-  { id: '7', nome: '🎤 Microfone Condensador', preco: 499.90 },
+export const produtos = [ 
+  { id: '1', nome: 'Fone Bluetooth', preco: 199.90, imagem: require('../assets/fone-sem-fio') },
+  { id: '2', nome: 'Teclado Mecânico', preco: 349.90, imagem: require('../assets/teclado.png') }, 
+  { id: '3', nome: 'Mouse Gamer', preco: 159.90, imagem: require('../assets/mouse.png') }, 
+  { id: '4', nome: 'Monitor Ultrawide', preco: 1299.90, imagem: require('../assets/monitor.png') }, 
+  { id: '5', nome: 'Controle sem Fio', preco: 299.90, imagem: require('../assets/controle-videogame.png') }, 
+  { id: '6', nome: 'Webcam Full HD', preco: 189.90, imagem: require('../assets/caixa-misteriosa.png') }, 
 ];
