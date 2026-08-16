7f74c422148434786013660d8efe3c9facb9b3d9
manoela-oliveira <manoela.oliveira0900@gmail.com>
2026-08-16 08:31:10 -0300

    ajustando tamanho produtos

--- mini-loja/data/produtos.js
+++ mini-loja/data/produtos.js
@@ -1,8 +1,8 @@
 export const produtos = [ 
-  { id: '1', nome: 'Fone Bluetooth', preco: 199.90, imagem: require('../assets/fone-sem-fio.png'), width: 50, height: 50},
-  { id: '2', nome: 'Celular Antigo', preco: 349.90, imagem: require('../assets/celular-antigo.png'), width: 60, height: 60},
-  { id: '3', nome: 'Mouse Gamer', preco: 159.90, imagem: require('../assets/mouse.png'), width: 55, height: 55},
-  { id: '4', nome: 'Monitor Ultrawide', preco: 1299.90, imagem: require('../assets/monitor.png'), width: 60, height: 60},
+  { id: '1', nome: 'Fone Bluetooth', preco: 199.90, imagem: require('../assets/fone-sem-fio.png'), width: 40, height: 40},
+  { id: '2', nome: 'Celular Antigo', preco: 349.90, imagem: require('../assets/celular-antigo.png'), width: 50, height: 50},
+  { id: '3', nome: 'Mouse Gamer', preco: 159.90, imagem: require('../assets/mouse.png'), width: 45, height: 45},
+  { id: '4', nome: 'Monitor Ultrawide', preco: 1299.90, imagem: require('../assets/monitor.png'), width: 50, height: 50},
   { id: '5', nome: 'Controle Bluetooth', preco: 299.90, imagem: require('../assets/controle-videogame.png'), width: 45, height: 40},
   { id: '6', nome: 'Caixinha Misteriosa', preco: 189.90, imagem: require('../assets/caixa-misteriosa.png'), width: 40, height: 40},
   { id: '7', nome: 'Câmera Profissional', preco: 189.90, imagem: require('../assets/camera-profissional.png'), width: 40, height: 40},
